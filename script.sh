f21c802812181df6e9dd37af7c6950c97f2db7c0
dj1ch <tg.1ch0712@gmail.com>
2023-11-24 14:35:03 -0800

    use both acm0 and usb0

--- script.sh
+++ script.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 # startup script
-minicom -D /dev/ACM0
+output=$(minicom -D /dev/ttyACM0 2>&1)
+
+if [[ $? -ne 0 && $output == "minicom: cannot open /dev/ttyACM0: No such file or directory" ]]; then
+  echo "Failed to open /dev/ttyACM0. Trying /dev/ttyUSB0..."
+
+  # try ttyUSB0
+  output=$(minicom -D /dev/ttyUSB0 2>&1)
+
+  if [[ $? -ne 0 ]]; then
+    echo "Failed to open /dev/ttyUSB0 and /dev/ttyACM0. Is the device properly attached?"
+    exit 1
+  fi
+fi
+
+# If the script reaches this point, the command was successful
+echo "Started successfully!"
